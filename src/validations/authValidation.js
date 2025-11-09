ba2b8ad52b7fd33b58b558b69069a011cdfdc983
Ruslan Tukhfyatullov <ruslan.tukhfyatullov@gmail.com>
2025-11-09 09:34:20 +0200

    fix:validation(login)

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -2,7 +2,6 @@ import { Joi, Segments } from 'celebrate';
 
 export const registerUserSchema = {
   [Segments.BODY]: Joi.object({
-    username: Joi.string(),
     email: Joi.string().required().email(),
     password: Joi.string().min(8).required(),
   }),
@@ -11,6 +10,6 @@ export const registerUserSchema = {
 export const loginUserSchema = {
   [Segments.BODY]: Joi.object({
     email: Joi.string().required().email(),
-    password: Joi.string().min(8).required(),
+    password: Joi.string().required(),
   }),
 };
